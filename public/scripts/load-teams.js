37289d2452aba9590807b9bdb0381f51465c37c9
=Cristian Traina <crissstian96@gmail.com>
2018-10-28 22:00:06 +0100

    second

--- public/scripts/load-teams.js
+++ public/scripts/load-teams.js
@@ -1,22 +1,46 @@
-fetch('/teams').then(_=>_.json()).then(teams => {
-  console.log(teams);
-  if (!teams || !teams.length) return;
-
-  var div = document.getElementById('team-container');
-
-  var clicks = {};
-  teams.forEach(team => {
-    var teamBtn = document.createElement('div');
-    teamBtn.classList = 'team-button';
-    teamBtn.addEventListener('click', function() {
-      clicks[team] = clicks[team] || 0;
-      document.getElementById('info-team').style.visibility = 'visible';
-      clicks[team]++;
-      if (clicks[team] === 2) {
-        alert('loadDocuments()');
-      }
+loadTeam();
+function loadTeam() {
+  fetch('/teams').then(_=>_.json()).then(teams => {
+    console.log(teams);
+    if (!teams || !teams.length) return;
+  
+    var div = document.getElementById('generated-content');
+    div.innerHTML = '';
+    var clicks = Array(teams.length);
+    document.querySelector('.button:nth-child(4) .icon').style.backgroundImage = 'url("../img/b4-off.png")';
+    document.querySelector('.button:nth-child(2) .icon').style.backgroundImage = 'url("../img/b2-on.png")';
+    teams.forEach((team, i) => {
+      var teamBtn = document.createElement('div');
+      teamBtn.classList = 'team-button';
+      clicks[i] = 0;
+      teamBtn.addEventListener('click', function() {
+        clicks[i]++;
+        document.getElementById('info-team').style.visibility = 'visible';
+        clicks[teamBtn]++;
+        if (clicks[i] === 2) {
+          console.log(document);
+          loadDocuments(document);
+        }
+      });
+      teamBtn.innerText = team.name;
+      div.appendChild(teamBtn);
     });
-    teamBtn.innerText = team.name;
-    div.appendChild(teamBtn);
   });
-});
+}
+
+function loadDocuments(document) {
+  console.log(document);
+  var div = document.getElementById('generated-content');
+  div.innerHTML = '';
+  var document = document.createElement('div');
+  document.classList = 'document';
+  div.appendChild(document);
+}
+
+
+function loadWorkflow() {
+  document.getElementById('generated-content').innerHTML = '';
+  document.getElementById('info-team').style.visibility = 'hidden';
+  document.querySelector('.button:nth-child(4) .icon').style.backgroundImage = 'url("../img/b4-on.png")';
+  document.querySelector('.button:nth-child(2) .icon').style.backgroundImage = 'url("../img/b2-off.png")';
+}
